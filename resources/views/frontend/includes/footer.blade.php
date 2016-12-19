2c1caadfaa497369246f6c75e2484b148c2c2b29
Halim <Halim@workstation>
2016-12-19 13:48:11 -0500

    work on footer

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,6 +1,98 @@
 <footer>
-     <div class="container" style="border: 1px solid red-">
-       @copyright
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 footerleft" style="border:0px solid red">
+                <div class="titlefooter"> Contact Us</div>
+                <p><i class="fa fa-map-marker"></i> 207 East 42 Street, 48th Floor, New York, NY 10017</p>
+                <p><i class="fa fa-phone"></i> Phone : +91 129 456 7890</p>
+                <p><i class="fa fa-envelope"></i> E-mail : halim@lardjane.com</p>
+            </div>
+
+            <div class="col-md-4 col-sm-6 footerleft" style="border:0px solid yellow">
+                <div class="titlefooter"> General Links</div>
+                <ul class="footer-ul">
+                    <li><a href="#"> Career</a></li>
+                    <li><a href="#"> Privacy Policy</a></li>
+                    <li><a href="#"> Terms & Conditions</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 col-sm-6 footerleft" style="border:0px solid orange">
+                <div class="titlefooter"> Our Mission</div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has 
+                    been the industry's.
+                </p>
+                <div class="social">
+                    <a href=""><i class="fa fa-facebook-official fa-2x"></i></a>
+                    <a href=""><i class="fa fa-twitter-square fa-2x"></i></a>
+                    <a href=""><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    <a href=""><i class="fa fa-linkedin-square fa-2x"></i></a>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
 
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>© 2016 - All Rights with H@lim Inc</p>
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<style>
+    footer { 
+        background-color:#0c1a1e; 
+        min-height:150px; 
+        border-top: 4px solid #fcab0e;
+        opacity: 0.9;
+    }
+    footer {
+        margin-top:30px; padding:25px 0px;
+    }
+    .titlefooter { margin-bottom:10px; font-size:25px; color:#fff; font-weight:700;}
+
+    footer p { color:#fff; font-size:12px !important; margin-bottom:10px;}
+    footer p i { width:16px; color:#999;}
+    footer a { color: #fff}
+    footer a:hover { color: #fff}
+    footer ul{
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    } 
+    footer ul li {
+        margin: 10px 2px;
+        font-size:12px;
+
+    }    
+    .social{
+        margin-top: 12px;
+    }
+    .social i{
+        margin-right: 5px;
+    }
+
+
+
+    .copyright { 
+        min-height:40px; 
+        background-color:#000000;
+        font-size: 12px;
+        opacity: 0.9;
+    }
+    .copyright p { text-align:left; color:#FFF; padding:10px 0; margin-bottom:0px;}
+
+
+
+
+
+</style>
---
+++ resources/views/frontend/includes/footer0.blade.php
@@ -0,0 +1,6 @@
+<footer>
+     <div class="container" style="border: 1px solid red-">
+       @copyright
+    </div>
+</footer>
+
