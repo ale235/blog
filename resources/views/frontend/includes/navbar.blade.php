16f17cbabf88243ba9464888f5ad985e2ec2f2d6
LARDJANE <halim.lardjane@gmail.com>
2016-12-19 16:59:52 -0500

    working on register/login

--- resources/views/frontend/includes/navbar.blade.php
+++ resources/views/frontend/includes/navbar.blade.php
@@ -36,8 +36,8 @@
         </div>
         <ul class="nav navbar-nav navbar-right"> 
             @if (Auth::guest())
-                <li><a href="{{ url('/login') }}">Login</a></li>
                 <li><a href="{{ url('/register') }}">Register</a></li>
+                <li><a href="{{ url('/login') }}">Login</a></li>
             @else
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->username }} <b class="caret"></b></a>
