a5ed5b019b9913b5dbd50bc538a3b6994cc4d28b
LARDJANE <halim.lardjane@gmail.com>
2016-12-19 18:07:41 -0500

    work on Socialite

--- config/services.php
+++ config/services.php
@@ -37,6 +37,7 @@ return [
     
     //https://www.youtube.com/watch?v=SvJ8cssqAo4
     //http://goodheads.io/2015/08/24/using-facebook-authentication-for-login-in-laravel-5/
+    //http://learninglaravel.net/create-a-user-authentication-system-with-facebook-and-socialite
     //http://goodheads.io/2015/08/24/using-twitter-authentication-for-login-in-laravel-5/
     //http://itsolutionstuff.com/post/laravel-5-login-with-linkedin-using-socialite-packageexample.html
     //https://blog.damirmiladinov.com/laravel/laravel-5.2-socialite-google-login.html#.WFhiPlxvDj8
