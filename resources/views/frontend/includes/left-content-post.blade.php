a8a05772d5e262758f4cbb02b58be05e88e8018f
Halim <Halim@workstation>
2016-12-20 02:37:05 -0500

    home page

---
+++ resources/views/frontend/includes/left-content-post.blade.php
@@ -0,0 +1,59 @@
+<div class="col-lg-8 left-content">
+
+    <div class="col-md-12- blogLong">
+        <h2><a href="{{ url('/post')}}">Title Blog 1</a></h2>
+        <div class="row row-article">
+            <div class="col-md-12">
+
+
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum. 
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum. 
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum. 
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
+                    five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+                    Lorem Ipsum. 
+                </p>
+
+
+
+            </div>
+        </div>
+        <div class="row row-info">
+            <div class="col-md-12">
+                <p>
+                    <i class="fa fa-user-o" aria-hidden="true"></i> by <a href="#">Maria</a> 
+                    | <i class="fa fa-calendar" aria-hidden="true"></i> Sept 17th, 2016
+                    | <i class="fa fa-comment" aria-hidden="true"></i> <a href="#">3 Comments</a>
+                    | <i class="fa fa-file-text-o" aria-hidden="true"></i> 53 Views
+                    | <i class="fa fa-share-alt-square" aria-hidden="true"></i> <a href="#">21 Shares</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    
+    <hr>
+    <div class="row" style="height: 50px; background: transparent">
+        fdfdfdfd
+    </div>
+
+    
+
+</div>
+
---
+++ resources/views/frontend/post.blade.php
@@ -0,0 +1,12 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+<div id="page-content" class="container" style="border: 0px solid red;">
+    <div class="row">
+        
+        @include('frontend.includes.left-content-post')
+        @include('frontend.includes.right-content')
+        
+    </div>
+</div>
+@endsection
