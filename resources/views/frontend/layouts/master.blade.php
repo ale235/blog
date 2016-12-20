8877a64d470eed3d9b63ce164ee1a8b6958a1b5b
LARDJANE <halim.lardjane@gmail.com>
2016-12-20 11:23:34 -0500

    remove #_=_ facebook redirect

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -31,6 +31,14 @@
                 history.pushState('', document.title, window.location.pathname); // nice and clean
                 e.preventDefault(); // no page reload
             }
+
+//            $(window).on('load', function (e) {
+//                if (window.location.hash == '#_=_') {
+//                    window.location.hash = ''; // for older browsers, leaves a # behind
+//                    history.pushState('', document.title, window.location.pathname); // nice and clean
+//                    e.preventDefault(); // no page reload
+//                }
+//            })
         </script>
     </head>
     <body>
