8c7af062995a3b221c0ce6da64768550590a934d
LARDJANE <halim.lardjane@gmail.com>
2016-12-21 15:51:07 -0500

    add name to user navbar admin

--- resources/views/backend/includes/navbar.blade.php
+++ resources/views/backend/includes/navbar.blade.php
@@ -131,7 +131,7 @@
         <!-- /.dropdown -->
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                <i class="fa fa-user fa-fw"></i>{{ Auth::user()->username }} <i class="fa fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu dropdown-user">
                 <li><a href="{{ url('/admin/profile') }}"><i class="fa fa-user fa-fw"></i> User Profile</a>
