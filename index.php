07f5a1d965eeab4f12a63a1af848af833b9836a5
ale235 <alejandrocolautti235@hotmail.com>
2017-10-29 22:45:42 -0300

    fvsvsfv
    vsssd

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+
+        <?php
+            echo "¡Hola, soy un script de PHP!";
+        ?>
+
+    </body>
+</html>
--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -12,7 +12,18 @@
             <p>Amante del mate en tacita, lector de sagas, mirador de series-anime-manga, horrible gamer, muy geek, colonista, casi ingeniero, loco, にほんごをべんきょう.</p>
 
             <p>Una presentación twitterezca para un gran fanático de esa red social :P.</p>
-        
+
+            Esteban F. Luengo.
+            Fanatico de los Broncos de Denver y Colon de Santa Fe. Tecnico Superior Organizador de Eventos( próximamente).
+            Me gustan los Juegos de Mesa, jugar Football Americano y juntarme con gente interesante.
+            Odio las charlas triviales y la venta de humo.
+            Me identificó con las ideologías de izquierda, pero trato de mantenerme objetivo en todos los asuntos.
+            Amigo de Alejandro Colautti desde 2013.
+
+            Santiago D Toller.
+            Fanatico de Glory Lamothe y Cloud 9. Me voy a recibir de la gente que hace analisis de sangre ( espero que no me toque lo otro). Me gusta el LoL, nadar y  Twitter. Odio que los vagos se queden en casa hasta las 3:00 am y caigan con cualquier random.
+            Me identificó con Ragnar, pero tambien lo banco a Rolo.
+            Amigo de Colautti desde 2013.
         </div>
         
         
