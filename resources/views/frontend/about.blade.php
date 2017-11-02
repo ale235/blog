c0074012fb2164f5161f964ee3f0deb7d8612dfb
ale235 <alejandrocolautti235@hotmail.com>
2017-11-01 22:41:06 -0300

    asdasd
    assadasd

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -3,30 +3,64 @@
 @section('content')
 <div id="page-content" class="container" style="border: 0px solid red;">
     <div class="row">
-        
-       
-        <div class="col-lg-12 left-content" style="padding-bottom: 60px;">
-            <h3 class="page-title">Sobre nosotros</h3>
-            <p>Vamos con la típica…</p>
 
-            <p>Amante del mate en tacita, lector de sagas, mirador de series-anime-manga, horrible gamer, muy geek, colonista, casi ingeniero, loco, にほんごをべんきょう.</p>
+        <div class="container">
 
-            <p>Una presentación twitterezca para un gran fanático de esa red social :P.</p>
+            <!-- Introduction Row -->
+            <h1 class="my-4">Sobre nosotros
+                {{--<small>It's Nice to Meet You!</small>--}}
+            </h1>
+            <p>Una breve presentación de los redactores de este Blog.</p>
 
-            Esteban F. Luengo.
-            Fanatico de los Broncos de Denver y Colon de Santa Fe. Tecnico Superior Organizador de Eventos( próximamente).
-            Me gustan los Juegos de Mesa, jugar Football Americano y juntarme con gente interesante.
-            Odio las charlas triviales y la venta de humo.
-            Me identificó con las ideologías de izquierda, pero trato de mantenerme objetivo en todos los asuntos.
-            Amigo de Alejandro Colautti desde 2013.
+            <!-- Team Members Row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="my-4">El Staff</h2>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center mb-4">
+                    <img class="rounded-circle img-fluid d-block mx-auto" width="200px" height="200px" src="/images/Santi.jpg"  alt="">
+                    <h3>Santiago Toller Achával
+                        {{--<small>Santiago Toller Achával</small>--}}
+                    </h3>
+                    <p>Oriundo de Federación pero estudiando Bioquímica en Santa Fe. Como me crie en el campo, siento que soy un intermedio entre sus costumbres y el ritmo de vida y pensar de la ciudad.  Me identifico fácilmente por portar un mate adonde quiera que vaya a estar más de una hora. Me gusta la música en casi todos sus géneros, los juegos de pc, el cine, las series, los viajes en el tiempo y la astrofísica . Soy de informarme de básicamente todo a través de Internet y de analizar en detalle las cosas antes de tomar una decisión o emitir opinión firme.</p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center mb-4">
+                    <img class="rounded-circle img-fluid d-block mx-auto" width="200px" height="200px" src="/images/Ale.jpg" alt="">
+                    <h3>Alejandro Colautti
+                        <small>ale235</small>
+                    </h3>
+                    <p>Santafesino muy orgulloso. Mis estudios primarios y secundarios fueron en la Dante Alighieri, donde conocí gente que afortunadamente sigo viendo al día de hoy.
+                    También muy orgulloso de haber estudiado en la UTN y muy contento donde trabajo hoy. Amante de la cultura Japonesa más que nada. Me encanta leer y ver series.
+                    Me gusta escribir pero me cuesta ponerme a hacerlo frecuentemente. Tuve varios blogs y espero que este sea el cimiento de uno muy bueno.
+                    </p>
+                </div>
+                <div class="col-lg-4 col-sm-6 text-center mb-4">
+                    <img class="rounded-circle img-fluid d-block mx-auto" width="200px" height="200px" src="/images/Chancho.jpg"  alt="">
+                    <h3>Esteban Luengo
+                        {{--<small>Job Title</small>--}}
+                    </h3>
+                    <p>            Fanático de los Broncos de Denver y Colón de Santa Fe. Técnico Superior Organizador de Eventos( próximamente).
+                        Me gustan los Juegos de Mesa, jugar Football Americano y juntarme con gente interesante.
+                        Odio las charlas triviales y la venta de humo.
+                        Me identifico con las ideologías de izquierda, pero trato de mantenerme objetivo en todos los asuntos.</p>
+                </div>
+            </div>
 
-            Santiago D Toller.
-            Fanatico de Glory Lamothe y Cloud 9. Me voy a recibir de la gente que hace analisis de sangre ( espero que no me toque lo otro). Me gusta el LoL, nadar y  Twitter. Odio que los vagos se queden en casa hasta las 3:00 am y caigan con cualquier random.
-            Me identificó con Ragnar, pero tambien lo banco a Rolo.
-            Amigo de Colautti desde 2013.
         </div>
         
         
     </div>
 </div>
 @endsection
+<style>
+    body {
+        padding-top: 54px;
+    }
+
+    @media (min-width: 992px) {
+        body {
+            padding-top: 56px;
+        }
+    }
+
+</style>
--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -21,23 +21,24 @@
 
                 <form role="form" method="POST" action="{{ url('/login') }}">
                     {{ csrf_field() }}    
-                    <h3 class="form-title">Login or <a href="{{ url('register') }}">Sign up</a></h3>
-                    <div class="row">
-                        <div class="col-lg-4 social_btn">
-                            <a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-facebook" role="button">Facebook</a>
-                        </div>
-                        <div class="col-lg-4 social_btn">
-                            <a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-twitter" role="button">Twitter</a>
-                        </div>	
-                        <div class="col-lg-4 social_btn">
-                            <a href="{{ url('/auth/google') }}" class="btn  btn-block btn-google" role="button">Google+</a>
-                        </div>	
-                    </div>
+                    <h3 class="form-title">Auntenticate</h3>
+                    {{--<h3 class="form-title">Auntenticate or <a href="{{ url('register') }}">Sign up</a></h3>--}}
+                    {{--<div class="row">--}}
+                        {{--<div class="col-lg-4 social_btn">--}}
+                            {{--<a href="{{ url('/auth/facebook') }}" class="btn btn-block btn-facebook" role="button">Facebook</a>--}}
+                        {{--</div>--}}
+                        {{--<div class="col-lg-4 social_btn">--}}
+                            {{--<a href="{{ url('/auth/twitter') }}" class="btn btn-block btn-twitter" role="button">Twitter</a>--}}
+                        {{--</div>	--}}
+                        {{--<div class="col-lg-4 social_btn">--}}
+                            {{--<a href="{{ url('/auth/google') }}" class="btn  btn-block btn-google" role="button">Google+</a>--}}
+                        {{--</div>	--}}
+                    {{--</div>--}}
 
-                    <div class="login-or">
-                        <hr class="hr-or">
-                        <span class="span-or">or</span>
-                    </div>
+                    {{--<div class="login-or">--}}
+                        {{--<hr class="hr-or">--}}
+                        {{--<span class="span-or">or</span>--}}
+                    {{--</div>--}}
 
                     <div class="row">
                         <div class="col-lg-12">	
@@ -51,16 +52,16 @@
                                 <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="1234">
                             </div>
                             <div class="" style="margin-top: 20px;">
-                                <button class="btn btn-primary btn-block" type="submit">Login</button>
+                                <button class="btn btn-primary btn-block" type="submit">Autenticate</button>
                             </div>
                             <div class="row" style="margin-top:8px;">
                                 <div class="col-lg-6">
                                     <label>
-                                        <input type="checkbox" value="remember-me" class=""> Remember Me
+                                        <input type="checkbox" value="remember-me" class=""> Recordar usuario y contraseña
                                     </label>
                                 </div>
                                 <div class="col-lg-6">
-                                    <p class="pull-right"><a href="{{ url('password/reset') }}">Forgot password?</a></p>
+                                    {{--<p class="pull-right"><a href="{{ url('password/reset') }}">Forgot password?</a></p>--}}
                                 </div>
                             </div>	  
                         </div>
