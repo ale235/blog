672c94b95ce76d9f02638fb37568e849b17efaa3
acolautti <acolautti@santafe.gov.ar>
2018-08-10 13:57:42 -0300

    mnn
    nmn

--- database/_MPD/deltas.sql
+++ database/_MPD/deltas.sql
@@ -1,2 +1,9 @@
 ALTER TABLE `post` ADD `description` TEXT NULL AFTER `slug`;
 
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `categoria_id` INT NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` VARCHAR(45) NOT NULL,
+  `descripcion_categoria` VARCHAR(145) NOT NULL,
+  PRIMARY KEY (`categoria_id`),
+  UNIQUE INDEX `nombre_categoria_UNIQUE` (`nombre_categoria` ASC))
+ENGINE = InnoDB;
