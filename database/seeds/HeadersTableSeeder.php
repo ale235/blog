9bfa7acd8218eedbbdf3d49ec695e8fe67c6be80
acolautti <acolautti@santafe.gov.ar>
2019-05-03 14:07:02 -0300

    ghjj
    jh

---
+++ database/seeds/HeadersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Header;
+use Illuminate\Database\Seeder;
+
+class HeadersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Header::create([
+            'image_path' => '/photos/shares/headers/welcome.png',
+            'style_type' => 1
+        ]);
+    }
+}
