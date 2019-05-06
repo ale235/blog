4c6df980498169ecbf86cb994d3dfd5d41463821
ale235 <alejandrocolautti235@hotmail.com>
2019-05-06 16:43:45 -0300

    fsdf
    sdfs

---
+++ resources/views/frontend/includes/galeria/index.blade.php
@@ -0,0 +1,158 @@
+<section id="blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="single-blog">
+                    <article>
+                        <section id="slider">
+                            <hr>
+                            <hr>
+                            <h4 class="post-title" style="    font-size: 25px;"><a href="">CONJUNTO SG</a></h4>
+                            <hr>
+                            <div id="home-carousel" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="item active" id="imagen1carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN01.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen2carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN02.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen3carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN03.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen4carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN04.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen5carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN05.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen6carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN06.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen7carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN07.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen8carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN08.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen9carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN09.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen10carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN10.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen11carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN11.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen12carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN12.jpg" alt="">
+                                    </div>
+                                    <div class="item " id="imagen13carru">
+                                        <img class="img-responsive" src="images/works/conjuntosg/SG-IMAGEN13.jpg" alt="">
+                                    </div>
+
+                                    <a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                                    <a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a>
+                                </div>
+                            </div> <!--/#home-carousel-->
+                        </section>
+                        <div style="    margin-top: -60px;">
+                            <h4 class="post-title">MEMORIA DESCRIPTIVA</a></h4>
+                            <hr>
+                            <div class="post-article" style="color: black">
+                                <br>&nbsp; Emplazado en un nuevo loteo de la ciudad de San Guillermo, el conjunto de viviendas persigue el máximo aprovechamiento de la superficie edificable y la posibilidad de repetirse en un total de tres lotes contiguos. Es así que se optó por la distribución de tres unidades habitacionales de un dormitorio cada una, distribuidos a lo largo de un espacio común lineal recostado sobre una de las medianeras. A los fines de una buena funcionalidad interior, así como también de una iluminación y ventilación natural óptima, cada unidad se desarrolla en L, volcando los espacios de servicio al ya mencionado espacio común y por otro lado, los locales habitables hacia un patio común de uso exclusivo, dotándolos de mayor privacidad. Hacia el frente del lote, el conjunto se completa con las cocheras (una para cada unidad de vivienda) y el palier, el cual nuclea los servicios comunes de gas (bloque lateral) y de agua (bloque en altura).
+                                <br>&nbsp; El principal desafío consistió en un desarrollo morfológico que responda a la lógica de cada uno de los conjuntos, pero que a su vez pueda replicarse de manera contigua y presentar una relación a escala urbana mayor. Por esta razón se adoptó la jerarquización de los ingresos partiendo de un bloque de mayor altura de carácter vertical y un juego de encastre con la linealidad y horizontalidad del área de cocheras que se reposa en un muro mínimo de apoyo. La separación de ambos se trabaja de manera virtual por medio del bloque de color que aloja la instalación de gas. De esta manera cada conjunto se define a si mismo sin dejar de dialogar con el total de la intervención.
+                                <br>&nbsp; Cabe destacar que la misma secuencia de ingreso común se replica en cada una de las unidades, individualizando el acceso por medio de un semicubierto que proteje al usuario y al área de servicios particulares. Interiormente la unidad busca economizar en relación a las instalaciones nucleando en pocos metros cuadrados el baño, lavadero y cocina, liberando la mayor cantidad de metros cuadrados al uso de espacios servidos.
+                                <br>  En cuanto a la materialidad, el conjunto se basa en una sintesis en el sentido que el uso de materiales se limita a superficies verticales blancas, cielorrasos de madera y pisos de cemento alisado. La única particularidad se da en el uso del color (dentro de una misma gama) para contribuir a diferenciar un conjunto del otro.
+                                <br>
+                                <br>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="sidebar-widget" style="    margin-top: 200px;">
+                    <h4 class="sidebar-title">Datos</h4>
+                    <ul>
+                        <li><a href="">Ubicación: San Guillermo, Santa Fe</a></li>
+                        <li><a href="">156 m<sup>2</sup>cubiertos</a></li>
+                        <li><a href="">54 m<sup>2</sup>semicubiertos</a></a></li>
+                        <li><a href="">Año: 2016</a></li>
+                        <li><a href="">Etapa: En Construcción</a></li>
+                    </ul>
+                </div>
+                <div class="sidebar-widget">
+                    <h4 class="sidebar-title">Galería</h4>
+                    <ul class="content-flickr">
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen1carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN01.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen2carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN02.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen3carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN03.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen4carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN04.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen5carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN05.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen6carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN06.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen7carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN07.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen8carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN08.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen9carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN09.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen10carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN10.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen11carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN11.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen12carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN12.jpg" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                <img class="img-responsive imagen13carru imgchiquita" src="images/works/conjuntosg/SG-IMAGEN13.jpg" alt="">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
